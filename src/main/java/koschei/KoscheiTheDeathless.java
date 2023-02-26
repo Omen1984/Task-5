a2b92106cdbec4efde158d6f8cc0c620d5eaea19
Omen1984 <ivan.plehanov42@yandex.ru>
2023-02-26 17:37:40 +0500

    Task complited

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,5 +1,6 @@
 package koschei;
 
+import koschei.models.Duck5;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,19 @@ import org.springframework.stereotype.Component;
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
+    private Duck5 duck;
 
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан , " + ocean.toString() + duck.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+
+    @Autowired
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
     }
 }
